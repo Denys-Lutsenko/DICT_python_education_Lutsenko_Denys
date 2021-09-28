5c099d3dedfa493e6b7d5a2678e59a7799e6ae93
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2021-09-28 23:17:08 +0300

    ChatBot 4-st

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,7 @@ print("Hello! My name is Alex")
 print("I was created in 2021")
 
 while True:
-    a = input("Enter text:")
+    a = input()
     g = 'hello' and 'hi'
     if a != g:
         print('And say hello or hi?!')
@@ -16,4 +16,11 @@ while True:
         remainder7 = int(input())
         age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
         print("Your age is ", age, " that's a good time to start programming!")
+        print('Now I will prove to you that I can count to any number you want. You want to test?')
+        number = int(input())
+        i = None
+        for i in range(number + 1):
+            print(i, "!")
+        else:
+            print( 'Completed, have a nice day!')
         break
