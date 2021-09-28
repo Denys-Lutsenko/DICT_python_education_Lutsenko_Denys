f22e99438cf7142c385a0adbf97190c22b540990
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2021-09-28 21:06:56 +0300

    ChatBot 3-st

--- chatbot.py
+++ chatbot.py
@@ -9,4 +9,11 @@ while True:
     if 'hello' and 'hi' in a:
         b = input("Please, remind me your name: ")
         print('What a great name you have, ', b, '!')
+        print("Let me guess your age. ")
+        print("Enter remainders of dividing your age by 3, 5 and 7:")
+        remainder3 = int(input())
+        remainder5 = int(input())
+        remainder7 = int(input())
+        age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+        print("Your age is ", age, " that's a good time to start programming!")
         break
