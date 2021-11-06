8a96a8358cd8dfa41a844af4f6ae0eeb4564de08
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2021-11-07 00:08:31 +0200

    CoffeeMachine 4-st

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -1,14 +1,15 @@
 # CoffeeMachine 1-st
 
-print('Starting to make a coffee\nGrinding coffee beans\nBoiling water\n'
-      'Mixing boiled water with crushed coffee beans\nPouring coffee into the cup\n'
-      'Pouring some milk into the cup\nCoffee is ready!')
+# print('Starting to make a coffee\nGrinding coffee beans\nBoiling water\n'
+#       'Mixing boiled water with crushed coffee beans\nPouring coffee into the cup\n'
+#       'Pouring some milk into the cup\nCoffee is ready!')
 
 # CoffeeMachine 2-st
 
-numb_of_water = 200
-numb_of_milk = 50
-numb_of_beans = 15
+
+# numb_of_water = 200
+# numb_of_milk = 50
+# numb_of_beans = 15
 
 # cup_counter = int(input("Write how many cups of coffee you will need: "))
 # water = cup_counter*numb_of_water
@@ -22,18 +23,108 @@ numb_of_beans = 15
 
 # CoffeeMachine 3-st
 
-water = int(input("Enter how much water: "))
-milk = int(input("Enter how much milk: "))
-beans = int(input("Enter how much beans: "))
-cup_counter = int(input("Write how many cups of coffee you will need: "))
-var_water = water // numb_of_water
-var_milk = milk // numb_of_milk
-var_beans = beans // numb_of_beans
-numb = min([var_water, var_milk, var_beans])
-print(numb)
-if numb == cup_counter:
-    print("Yes, I can make that amount of coffee")
-if numb > cup_counter:
-    print("Yes, I can make that amount of coffee (and even " + str(numb - cup_counter) + " more than that)")
-else:
-    print("No, I can make only " + str(numb) + " cups of coffee")
+# numb_of_water = 200
+# numb_of_milk = 50
+# numb_of_beans = 15
+
+# water = int(input("Enter how much water: "))
+# milk = int(input("Enter how much milk: "))
+# beans = int(input("Enter how much beans: "))
+# cup_counter = int(input("Write how many cups of coffee you will need: "))
+# var_water = water // numb_of_water
+# var_milk = milk // numb_of_milk
+# var_beans = beans // numb_of_beans
+# numb = min([var_water, var_milk, var_beans])
+# print(numb)
+# if numb == cup_counter:
+#     print("Yes, I can make that amount of coffee")
+# elif numb > cup_counter:
+#     print("Yes, I can make that amount of coffee (and even " + str(numb - cup_counter) + " more than that)")
+# else:
+#     print("No, I can make only " + str(numb) + " cups of coffee")
+
+# CoffeeMachine 4-st
+
+water = 400
+milk = 540
+beans = 120
+cups = 9
+money = 550
+
+
+def content():
+        print('The coffee machine has:')
+        print()
+        print(f'{water} of water')
+        print(f'{milk} of milk')
+        print(f'{beans} of coffee beans')
+        print(f'{cups} of disposable cups')
+        print(f'{money} of money')
+        print()
+
+content()
+
+def select_coffe() -> int:
+    return int(input('What do you want to buy?'
+                     ' 1 - espresso, 2 - latte, 3 - cappuccino: '))
+def buy():
+        global water
+        global milk
+        global beans
+        global money
+        global cups
+        user_coffee = select_coffe()
+        if user_coffee == 1:            #espresso
+            water -= 250
+            beans -= 16
+            cups -= 1
+            money += 4
+        elif user_coffee == 2:          #latte
+            water -= 350
+            milk -= 75
+            beans -= 20
+            cups -= 1
+            money += 7
+        elif user_coffee == 3:          #cappuccino
+            water -= 200
+            milk -= 100
+            beans -= 12
+            cups -= 1
+            money += 6
+
+
+def fill():
+        global water
+        global milk
+        global beans
+        global money
+        global cups
+        add_water = int(input("Write how many ml of water you want to add: "))
+        water += add_water
+        add_milk = int(input("Write how many ml of milk you want to add: "))
+        milk += add_milk
+        add_beans = int(input("Write how many grams of coffee beans you want to add: "))
+        beans += add_beans
+        add_cups = int(input("Write how many disposable coffee cups you want to add: "))
+        cups += add_cups
+
+
+def take():
+        global money
+        print(f'I gave you "{money}')
+        money = 0
+
+
+def order():
+      choice_to_order = input('Write action (1.buy, 2.fill, 3.take): ')
+      if choice_to_order == "1":
+        buy()
+        content()
+      elif choice_to_order == "2":
+        fill()
+        content()
+      elif choice_to_order == "3":
+        take()
+        content()
+
+order()
