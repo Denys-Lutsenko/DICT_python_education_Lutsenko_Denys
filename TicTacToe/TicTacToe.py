64a1e48b3ce720d0dcab9036a4725aec6c68a197
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2021-11-10 13:55:18 +0200

    TicTacToe 2-st

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,3 +1,17 @@
-print("X O X")
-print("O X O")
-print("X X O")
+'''TicTacToe 1-st'''
+# print("X O X")
+# print("O X O")
+# print("X X O")
+
+'''TicTacToe 2-st'''
+# O_OXXO_XX
+# _XO__X___
+board = list(input('Enter cells: '))
+
+print("---------")
+print("| " + board[0] + " " + board[1] + " " + board[2] + " |")
+print("| " + board[3] + " " + board[4] + " " + board[5] + " |")
+print("| " + board[6] + " " + board[7] + " " + board[8] + " |")
+print("---------")
+
+
