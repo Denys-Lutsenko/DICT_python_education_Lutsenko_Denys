71e939fe11371caf08eee122c0095644418c5412
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2021-11-29 21:12:45 +0200

    DinnerParty 2-st

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -1,13 +1,22 @@
-'''DinnerParty 1-st'''
-num_of_friends = int(input("Enter the number of friends joining (including you): \n"))
+'''DinnerParty 2-st'''
+
+num_of_friends = int(input("Enter the number of friends joining (including you):\n"))
 print()
 friends_dictionary = {}
 
 if num_of_friends <= 0:
     print("No one is joining for the party")
 else:
-    print("Enter the name of every friend (including you), each on a new line:")
-    while len(friends_dictionary) != num_of_friends:
-        name = input("")
+
+    print("Enter the name of every friend (including you), each on a new line: ")
+    for i in range(num_of_friends):
+        name = input()
         friends_dictionary.update({name: 0})
+    bill = int(input("\nEnter the total bill: "))
+    if bill / num_of_friends % 10 == 0:
+        for i in friends_dictionary:
+            friends_dictionary[i] = int(bill / num_of_friends)
+    else:
+        for i in friends_dictionary:
+            friends_dictionary[i] = round(bill / num_of_friends, 2)
     print("\n{}".format(friends_dictionary))
