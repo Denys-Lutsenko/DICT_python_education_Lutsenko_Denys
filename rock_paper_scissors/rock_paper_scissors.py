0e293587964db00fe5e13c17e48a171466dbc3d0
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2021-12-17 17:34:20 +0200

    rock_paper_scissors 2-st

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,5 +1,24 @@
-'''rock_paper_scissors 1-st'''
+'''rock_paper_scissors 2-st'''
+from random import choice
+
 MOVES = ('rock', 'paper', 'scissors')
 WINNING_MOVES = {MOVES[i]: MOVES[(i + 1) % len(MOVES)]
                  for i, _ in enumerate(MOVES)}
-print(f'Sorry, but the computer chose {WINNING_MOVES[input()]}')
+
+
+def main():
+    user_move = input("Your choice: ")
+    computer_move = choice(MOVES)
+    if computer_move == user_move:
+        print(f'There is a draw {computer_move}')
+
+    elif computer_move == WINNING_MOVES[user_move]:
+        print(f'Sorry, but the computer chose {computer_move}')
+    else:
+        print(f'Well done. The computer chose {computer_move}',
+              'and failed')
+
+
+if __name__ == '__main__':
+    main()
+
