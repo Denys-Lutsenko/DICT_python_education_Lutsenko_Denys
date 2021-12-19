c7eefd400058c4e1b1995de4e870bc2ce9cf0e6c
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2021-12-19 23:32:29 +0200

    rock_paper_scissors 3-st

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,24 +1,26 @@
-'''rock_paper_scissors 2-st'''
+'''rock_paper_scissors 3-st'''
 from random import choice
-
-MOVES = ('rock', 'paper', 'scissors')
-WINNING_MOVES = {MOVES[i]: MOVES[(i + 1) % len(MOVES)]
-                 for i, _ in enumerate(MOVES)}
+MOVES = {'scissors': 'rock', 'paper': 'scissors',
+            'rock': 'paper'}
 
 
 def main():
-    user_move = input("Your choice: ")
-    computer_move = choice(MOVES)
-    if computer_move == user_move:
-        print(f'There is a draw {computer_move}')
-
-    elif computer_move == WINNING_MOVES[user_move]:
-        print(f'Sorry, but the computer chose {computer_move}')
-    else:
-        print(f'Well done. The computer chose {computer_move}',
-              'and failed')
+    while True:
+        user_move = input()
+        random_choice = choice(list(MOVES))
+        if user_move == '!exit':
+            print('Bye!')
+            break
+        if user_move not in list(MOVES):
+            print('Invalid input')
+            continue
+        if user_move == random_choice:
+            print(f'There is a draw ({random_choice})')
+        elif random_choice == MOVES[user_move]:
+            print(f'Sorry, but the computer chose {random_choice}')
+        else:
+            print(f'Well done. The computer chose {random_choice} and failed')
 
 
 if __name__ == '__main__':
     main()
-
