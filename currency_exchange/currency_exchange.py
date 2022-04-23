9f1a13190f58176df1e43947c5d928652a8f1938
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2022-04-23 07:14:27 +0300

    currency_exchange 2-st

--- currency_exchange/currency_exchange.py
+++ currency_exchange/currency_exchange.py
@@ -1,3 +1,6 @@
-coin = int(input("Please, enter the number of conicoins you have: "))
-rate = float(input("Please, enter the exchange rate: "))
-print(f"The total amount of dollars: {coin * rate}")
+rates = {"ARS": 0.82, "HNL": 0.17, "AUD": 1.9622, "MAD": 0.208}
+
+wallet = float(input())
+
+for i in rates:
+    print(f"I will get {round(rates[i] * wallet, 2)} {i} from the sale of {wallet} conicoins.")
