2afb0dfe8d1c4dd37d1e545024c90e151390c33f
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2022-04-24 01:05:10 +0300

    web_page_scraper 2-st

--- web_page_scraper/web_page_scraper.py
+++ web_page_scraper/web_page_scraper.py
@@ -1,6 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
+
 
 url = input("Input the URL: ")
-response = requests.get(url)
-print(response.json().get("content")) if "content" in response.json() and response.status_code == 200 \
-    else print("Invalid quote resource!")
+response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+content = BeautifulSoup(response.content, 'html.parser')
+title = content.title.text
+imdb = content.find('meta', {'property': "imdb:pageType"})
+description = content.find('meta', {'name': 'description'})
+if description is not None:
+    description = content.find('meta', {'name': 'description'}).get('content')
+total = {'title': title, 'description': description}
+if description is None or imdb is None:
+    print('Invalid movie page!')
+else:
+    print(total)
