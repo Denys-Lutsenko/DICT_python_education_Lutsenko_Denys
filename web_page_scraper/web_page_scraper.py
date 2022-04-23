d54ca5929afceb198e3912b77bd8904710dca57a
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2022-04-24 01:19:51 +0300

    web_page_scraper 3-st

--- web_page_scraper/web_page_scraper.py
+++ web_page_scraper/web_page_scraper.py
@@ -1,17 +1,13 @@
 import requests
-from bs4 import BeautifulSoup
 
-
+file = open("source.html", "wb")
 url = input("Input the URL: ")
-response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
-content = BeautifulSoup(response.content, 'html.parser')
-title = content.title.text
-imdb = content.find('meta', {'property': "imdb:pageType"})
-description = content.find('meta', {'name': 'description'})
-if description is not None:
-    description = content.find('meta', {'name': 'description'}).get('content')
-total = {'title': title, 'description': description}
-if description is None or imdb is None:
-    print('Invalid movie page!')
+response = requests.get(url)
+status_code = response.status_code
+if status_code == 200:
+    file.write(response.content)
+    print("Content saved.")
 else:
-    print(total)
+    print(f"The URL returned {status_code}!")
+
+file.close()
