7be7b38d07abb24ad518013989dee5c5d46948b2
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2022-06-03 23:42:10 +0300

    regular_expressions 4-st

--- regular_expressions/regular_expressions.py
+++ regular_expressions/regular_expressions.py
@@ -23,4 +23,21 @@ def unequal_len_regex(regex, string):
         return unequal_len_regex(regex, string[1:])
 
 
-print(unequal_len_regex(regex_string, input_string))
+def regex_v3(regex: str, string):
+    if regex.startswith("^"):
+        regex = regex.replace("^", "")
+        temp_regex = regex.replace("$", "")
+        for i in range(len(temp_regex)):
+            if temp_regex[i] != "." and temp_regex[i] != string[i]:
+                return False
+
+    if regex.endswith("$"):
+        regex = regex.replace("$", "")
+        for i in range(-1, -1 - len(regex), -1):
+            if regex[i] != "." and regex[i] != string[i]:
+                return False
+
+    return unequal_len_regex(regex, string)
+
+
+print(regex_v3(regex_string, input_string))
