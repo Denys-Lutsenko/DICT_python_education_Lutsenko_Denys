122573cc6ccd6b827fa9fb098cf32eceb828be83
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2022-06-03 23:40:32 +0300

    regular_expressions 2-st

--- regular_expressions/regular_expressions.py
+++ regular_expressions/regular_expressions.py
@@ -1,10 +1,15 @@
-regex, input_ = input().split("|")
+regex_string, input_string = input().split("|")
 
-if regex == "":
-    print(True)
-elif input_ == "":
-    print(False)
-elif regex == ".":
-    print(True)
-else:
-    print(regex == input_)
+
+def regex_recursion(regex, string):
+    if regex == "":
+        return True
+    elif string == "":
+        return False
+    elif regex[0] != "." and regex[0] != string[0]:
+        return False
+    else:
+        return regex_recursion(regex[1:], string[1:])
+
+
+print(regex_recursion(regex_string, input_string))
